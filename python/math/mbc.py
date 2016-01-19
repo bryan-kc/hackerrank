3ea8d4bf1122388024189873154391a6c6a78c73
bryan-kc <sendtobryanchiu@gmail.com>
2016-01-19 13:49:41 -0800

    cleaned up solution. Still thinking of how to determine angle using known identities.

--- python/math/mbc.py
+++ python/math/mbc.py
@@ -1,10 +1,15 @@
 import numpy as np
-
+import math as m
 
 ab = (raw_input('Give line AB :'))
 bc = int(raw_input('Give length of line BC :'))
 
-ac = sqrt(ab**2 + bc**2)
+## length of hypotenuse of right triangle
+ac = m.hypot(ab,bc)
 
-M = 0.5* ac
+## solve for phi, angle made from MC, BC ##
+phi = m.radians(m.acos((bc/ac)))
+
+## midpoint to C
+mc = 0.5 * ac
 
